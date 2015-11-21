e9e9827bb12b47ff1a939bcd3e3c93f62634998f
mcavataio <Mcavataiosfp@gmail.com>
2015-11-21 13:59:25 -0800

    Edit to Snippets

--- client/exSnippets/content.js
+++ client/exSnippets/content.js
@@ -1,29 +1,56 @@
-onload = function() {
-  var login = document.getElementById("login");
-  var output = document.getElementById("output");
-  var clientId = '9a1a341bb29b6bf5b13e';
+  // var redirectUri = chrome.identity.getRedirectURL() + "github"
+  // var clientId = '9a1a341bb29b6bf5b13e';
+  // var authUrl = "https://github.com/oauth/authorize/?" +
+  //     "client_id=" + clientId + "&" +
+  //     "redirect_uri=" + encodeURIComponent(redirectUri) +
+  //     "&response_type=token"
 
-  login.onclick = function() {
-    var redirectUrl = chrome.identity.getRedirectURL();
-    var clientId = '9a1a341bb29b6bf5b13e';
-    var authUrl = "https://github.com/oauth/authorize/?" +
-        "client_id=" + clientId + "&" +
-        "response_type=token&" +
-        "redirect_uri=" + encodeURIComponent(redirectUrl);
+
+  // chrome.identity.launchWebAuthFlow({url: "authUrl", interactive: true},
+  //     function(responseUrl) {
+  //       console.log(responseUrl);
+  //       // var accessToken = responseUrl.substring(responseUrl.indexOf("=") + 1);
+  //       // console.log(accessToken);
+
+
+  //     })
+  //   })
+
+
+
+
+
+
+
+
+
+
+// onload = function() {
+//   var login = document.getElementById("login");
+//   var output = document.getElementById("output");
+//   var clientId = '9a1a341bb29b6bf5b13e';
+
+//   login.onclick = function() {
+//     var redirectUrl = chrome.identity.getRedirectURL();
+//     var clientId = '9a1a341bb29b6bf5b13e';
+//     var authUrl = "https://github.com/oauth/authorize/?" +
+//         "client_id=" + clientId + "&" +
+//         "response_type=token&" +
+//         "redirect_uri=" + encodeURIComponent(redirectUrl);
  
-    chrome.identity.launchWebAuthFlow({url: authUrl, interactive: true},
-        function(responseUrl) {
-      console.log(responseUrl);
-      var accessToken = responseUrl.substring(responseUrl.indexOf("=") + 1);
-      console.log(accessToken);
+//     chrome.identity.launchWebAuthFlow({url: authUrl, interactive: true},
+//         function(responseUrl) {
+//       console.log(responseUrl);
+//       var accessToken = responseUrl.substring(responseUrl.indexOf("=") + 1);
+//       console.log(accessToken);
 
-      // var api = new InstagramAPI(accessToken);
-      // api.request("users/self/feed", undefined, function(data) {  
-      //   console.log(data);
-      //   output.textContent = JSON.stringify(data, null, 4);
+//       var api = new InstagramAPI(accessToken);
+//       api.request("users/self/feed", undefined, function(data) {  
+//         console.log(data);
+//         output.textContent = JSON.stringify(data, null, 4);
         
 
-      });
-    });
-  };
-};
+//       });
+//     });
+//   };
+// };
--- client/exSnippets/main.js
+++ client/exSnippets/main.js
@@ -1,37 +1,5 @@
 var blue = $('p').val()
 $('document').ready(function() {
-
-  // chrome.identity.launchWebAuthFlow(
-  // {'url': 'https://github.com/login/oauth/authorize', 'interactive': true},
-  // function(redirect_url) { console.log(redirect_url)})
-  // $.ajax({
-  //   type: 'POST',
-  //   url: 'http://localhost:3000/api/snippets',
-  //   data: data
-  //   }
-  // })
-onload = function() {
-  var login = document.getElementById("login");
-  var output = document.getElementById("output");
-  var clientId = '9a1a341bb29b6bf5b13e';
-
-  login.onclick = function() {
-    var redirectUrl = chrome.identity.getRedirectURL();
-    var clientId = '9a1a341bb29b6bf5b13e';
-    var authUrl = "https://github.com/oauth/authorize/?" +
-        "client_id=" + clientId + "&" +
-        "response_type=token&" +
-        "redirect_uri=" + encodeURIComponent(redirectUrl);
- 
-    chrome.identity.launchWebAuthFlow({url: authUrl, interactive: true},
-        function(responseUrl) {
-          console.log(responseUrl);
-          var accessToken = responseUrl.substring(responseUrl.indexOf("=") + 1);
-          console.log(accessToken);
-
-    })
-  }
-}
   $('.addSnippet').click(function() {
     var text = $('#dropBox').val();
     $.ajax({
@@ -46,3 +14,37 @@ onload = function() {
     })
   })
 })
+
+  // chrome.identity.launchWebAuthFlow(
+  // {'url': 'https://github.com/login/oauth/authorize', 'interactive': true},
+  // function(redirect_url) { console.log(redirect_url)})
+  // $.ajax({
+  //   type: 'POST',
+  //   url: 'http://localhost:3000/api/snippets',
+  //   data: data
+  //   }
+//   // })
+// onload = function() {
+//   var login = document.getElementById("login");
+//   var output = document.getElementById("output");
+//   var clientId = '9a1a341bb29b6bf5b13e';
+
+//   login.onclick = function() {
+  // var redirectUri = chrome.identity.getRedirectURL() + "github"
+  // var clientId = '9a1a341bb29b6bf5b13e';
+  // var authUrl = "https://github.com/oauth/authorize/?" +
+  //     "client_id=" + clientId + "&" +
+  //     "redirect_uri=" + encodeURIComponent(redirectUri) +
+  //     "&response_type=token"
+
+  // chrome.identity.launchWebAuthFlow({url: "https://github.com/oauth/authorize/?client_id=9a1a341bb29b6bf5b13e&redirect_uri=" + 
+  //   encodeURIComponent(redirectUri) + "$response_type=token", interactive: true},
+  //     function(responseUrl) {
+  //       console.log(responseUrl);
+  //       console.log("hello")
+  //       // var accessToken = responseUrl.substring(responseUrl.indexOf("=") + 1);
+  //       // console.log(accessToken);
+  //     })
+  //   })
+
+// })
